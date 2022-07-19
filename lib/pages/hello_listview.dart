85e61f81e99b621bbd8bb979c64946272b8b45d9
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-07-19 16:15:46 -0300

    ListView ou GridView Funcionando

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -14,8 +14,7 @@ class HelloListView extends StatefulWidget {
 }
 
 class _HelloListViewState extends State<HelloListView> {
-
-  bool gridView = true;
+  bool _gridView = true;
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +23,19 @@ class _HelloListViewState extends State<HelloListView> {
         title: Text('ListView'),
         actions: [
           IconButton(
-            icon: Icon(Icons.list),onPressed: () {
-              print('Lista');
-              gridView = false;
+            icon: Icon(Icons.list), onPressed: () {
+            print('Lista');
+            setState((){
+              _gridView = false;
+            });
           },
           ),
           IconButton(
-            icon: Icon(Icons.grid_on),onPressed: () {
+            icon: Icon(Icons.grid_on), onPressed: () {
             print('Grid');
-            gridView = true;
+            setState((){
+              _gridView = true;
+            });
           },
           )
         ],
@@ -40,53 +43,69 @@ class _HelloListViewState extends State<HelloListView> {
       body: _body(),
     );
   }
-}
 
- _body() {
-  List<Dog> dogs = [
-    new Dog("nome1","assets/images/dog1.jpg"),
-    new Dog("nome2","assets/images/dog2.jpg"),
-    new Dog("nome3","assets/images/dog3.jpg"),
-    Dog("nome4","assets/images/dog4.jpg"),
-    Dog("nome5","assets/images/dog5.jpg"),
-   ];
 
-  return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemCount: dogs.length, //tamanho da lista
-      itemBuilder: (BuildContext context, int index){
-        Dog dog = dogs[index];
-        return Stack(
-          fit:StackFit.expand,
-          children: [
-            _img(dog.foto),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.circular(16)
-                ),
-                child: Text(
-                  dog.nome,
-                  style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.deepOrange
-                  ),
-                ),
+  _body() {
+    List<Dog> dogs = [
+      new Dog("nome1", "assets/images/dog1.jpg"),
+      new Dog("nome2", "assets/images/dog2.jpg"),
+      new Dog("nome3", "assets/images/dog3.jpg"),
+      Dog("nome4", "assets/images/dog4.jpg"),
+      Dog("nome5", "assets/images/dog5.jpg"),
+    ];
+
+    if (_gridView) {
+      return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: dogs.length, //tamanho da lista
+          itemBuilder: (BuildContext context, int index) {
+            return _itemView(dogs, index);
+          }
+      );
+    } else {
+      return ListView.builder(
+          itemExtent: 350,
+          itemCount: dogs.length, //tamanho da lista
+          itemBuilder: (BuildContext context, int index) {
+            return _itemView(dogs, index);
+          }
+      );
+    }
+  }
+
+  Stack _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index];
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        _img(dog.foto),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.circular(16)
+            ),
+            child: Text(
+              dog.nome,
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.deepOrange
               ),
             ),
-          ],
-        );
-    }
-  );
-}
+          ),
+        ),
+      ],
+    );
+  }
 
-Image _img(String img_name) {
-  return Image.asset(
+  Image _img(String img_name) {
+    return Image.asset(
       img_name,
       fit: BoxFit.cover,
-  );
+    );
+  }
 }
